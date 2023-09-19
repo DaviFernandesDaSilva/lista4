d6a0e5a9b02de5c82e899da0987dd4e85e350da6
Davi Fernandes <83321222+DaviFernandesDaSilva@users.noreply.github.com>
2023-09-19 18:51:09 -0300

    Estrutura base da Lista 4 - 1/2

---
+++ lista4.py
@@ -0,0 +1,201 @@
+import time
+
+sair = 0
+
+while sair == 0:
+    time.sleep(0.5)
+
+    print("\n\nExercicios:")
+    print("0 - Sair")
+    print("1 - Meu Curso é Show 10x")
+    print("2 - Ler inteiro, Estou sabendo Programar haha")
+    print("3 - Imprimir números na tela até 100")
+    print("4 - Imprimir 500,497,494 ... 2 na tela")
+    print("5 - De 1000 em 1000")
+    print("6 - Ler 10 valores e exibir soma")
+    print("7 - Ler 10 valores e exibir média")
+    print("8 - Ler 10 valores positivos, ignorando os negativos e exibir média")
+    print("9 - Ler 10 valores e exibir o maior e o menor valor recebido")
+    print("10 - Ler valor inteiro N")
+    print("11 - Exibir soma dos 50 primeiros números pares")
+    print("12 - Calcular Fatorial")
+    print("13 - Exibir divisores")
+    print("14 - Primo ou não")
+    print("15 - Ler certa quantidade de números e exibir o maior deles")
+    print("16 - ")
+    print("17 - ")
+    print("18 - ")
+
+    escolha = int(input("Executar: "))
+
+    # =======================================
+    # SAIR ==================================
+    if escolha == 0:
+        print("Programa finalizado.")
+
+        break
+
+    # =======================================
+    # QUESTÃO 1 =============================
+
+    if escolha == 1:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 2 =============================
+
+    elif escolha == 2:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 3 =============================
+
+    elif escolha == 3:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 4 =============================
+
+    elif escolha == 4:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 5 =============================
+
+    elif escolha == 5:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 6 =============================
+
+    elif escolha == 6:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 7 =============================
+
+    elif escolha == 7:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 8 =============================
+
+    elif escolha == 8:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 9 =============================
+
+    elif escolha == 9:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 10 ============================
+
+    elif escolha == 10:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 11 ============================
+
+    elif escolha == 11:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 12 ============================
+
+    elif escolha == 12:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 13 ============================
+
+    elif escolha == 13:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 14 ============================
+
+    elif escolha == 14:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 15 ============================
+
+    elif escolha == 15:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 16 ============================
+
+    elif escolha == 16:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 17 ============================
+
+    elif escolha == 17:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 18 ============================
+
+    elif escolha == 18:
+        print("")
+    
+    # =======================================
+    # QUESTÃO 19 ============================
+
+    elif escolha == 19:
+        print("")
+
+    # =======================================
+    # QUESTÃO 20 ============================
+
+    elif escolha == 20:
+        print("")
+
+    # =======================================
+    # QUESTÃO 21 ============================
+
+    elif escolha == 21:
+        print("")
+
+    # =======================================
+    # QUESTÃO 22 ============================
+
+    elif escolha == 22:
+        print("")
+
+    # =======================================
+    # QUESTÃO 23 ============================
+
+    elif escolha == 23:
+        print("")
+
+    # =======================================
+    # QUESTÃO 24 ============================
+
+    elif escolha == 24:
+        print("")
+
+    # =======================================
+    # QUESTÃO 25 ============================
+    elif escolha == 25:
+        print("")
+
+    # =======================================
+    # QUESTÃO 26 ============================
+
+    elif escolha == 26:
+        print("")
+    # =======================================
+    # QUESTÃO 27 ============================
+
+    elif escolha == 27:
+        print("")
+
+    # =======================================
+
+    else:
+        print("Deus é bom")
